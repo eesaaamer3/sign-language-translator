43d822147afccd2e05a586e27cdc78625b82e92f
Eesa Aamer <eesaaamer5@gmail.com>
2021-01-03 17:59:54 -0500

    Webcam issues fixed

--- main.py
+++ main.py
@@ -2,24 +2,21 @@ import cv2
 import numpy as np
 import keras 
 
-
-
 webcam_capture = cv2.VideoCapture(0)
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
-while True:
+while webcam_capture.isOpened():
     ret, frame = webcam_capture.read()
+    if ret == True: 
+        fgmask = fgbg.apply(frame)
 
-    fgmask = fgbg.apply(frame)
+        cv2.imshow("fgbg", fgmask)
+        cv2.imshow("frame", frame)
 
-    cv2.imshow("fgmask", fgmask)
-    cv2.imshow("frame", frame)
+        key = cv2.waitKey(0) & 0xFF
 
-    k = cv2.waitKey(30) & 0xff
-    if k ==  27:
-        break
+        if key == ord('q'):
+            break
 
 webcam_capture.release()
 cv2.destroyAllWindows()
-
-    
